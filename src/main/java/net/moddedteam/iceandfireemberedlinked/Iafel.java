547c072e4fde7b835b7702525c1dd0d61e995871
Nathan REBEILLEAU <nathan.rebeilleau@gmail.com>
2025-02-24 20:20:35 +0100

    mise en place finale est intégration dess mods

--- src/main/java/net/moddedteam/iceandfireemberedlinked/Iafel.java
+++ src/main/java/net/moddedteam/iceandfireemberedlinked/Iafel.java
@@ -52,6 +52,15 @@ public class Iafel
 
     }
 
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public class ModEventSubscriber {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            EmberedLinkedIntegration.logModStatus();
+        }
+    }
+
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
